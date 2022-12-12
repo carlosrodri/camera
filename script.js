c4227e3e2b93e5d5daa586d84fbcdf32883c692d
carlos.rodriguez@grupodigital.co <carlos.rodriguez@grupodigital.co>
2022-12-12 11:58:31 -0500

    initial commit

--- script.js
+++ script.js
@@ -10,13 +10,29 @@ let streamStarted = false;
 
 const [play, pause, screenshot] = buttons;
 
-screen.addEventListener("orientationchange", () => {
-  console.log(`The orientation of the screen is: ${screen.orientation}`);
+const log = document.getElementById("log");
+
+// Lock button: Lock the screen to the other orientation (rotated by 90 degrees)
+const rotate_btn = document.querySelector("#lock_button");
+rotate_btn.addEventListener("click", () => {
+  log.textContent += `Lock pressed \n`;
+
+  const oppositeOrientation = screen.orientation.type.startsWith("portrait")
+    ? "landscape"
+    : "portrait";
+  screen.orientation
+    .lock(oppositeOrientation)
+    .then(() => {
+      log.textContent = `Locked to ${oppositeOrientation}\n`;
+    })
+    .catch((error) => {
+      log.textContent += `${error}\n`;
+    });
 });
 
 const constraints = {
   video: {
-    aspectRatio: 9 / 16,
+    aspectRatio: 16 / 9,
     width: {
       min: 1280,
       ideal: 1920,
